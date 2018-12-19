711b84e7337318860a4fb14fb0a8fffd28850e52
leetoya <leetoya@naver.com>
2018-12-20 00:04:35 +0900

    ets

--- csharp_test/chapter3/example16.cs
+++ csharp_test/chapter3/example16.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace csharp_test.chapter3
+namespace Example_15
 {
-    class example16
+    public class Foo
     {
+        private decimal x;
+        public decimal X
+        {
+            get { return x; }
+            private set { x = Math.Round(value, 2); }
+        }
+        public decimal Round(decimal x)
+        {
+            X = x;
+            return X;
+        }
+
     }
+    //class program
+    //{
+    //    static void main(string[] args)
+    //    {
+    //        foo foo = new foo();
+    //        foo.round(4.535m);
+    //        foo.x = 5.35m;// private이 set 앞에 있어 접근 불가
+    //        console.writeline($"{foo.x}");
+    //    }
+    //}
 }
