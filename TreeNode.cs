6fe760e3414722f97111ef4ed03d98ba715db6a5
leekeunha <leetoya@naver.com>
2019-05-11 19:19:38 +0900

    binarySearchTreeaa

---
+++ TreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySerachTreeLibrary
+{
+    public class TreeNode
+    {
+        public int _key;
+        public int _value;
+        public TreeNode _leftChild;
+        public TreeNode _rightChild;
+
+        public TreeNode(int key, int value)
+        {
+            _key = key;
+            _value = value;
+        }
+    }
+}
